56f5a97711ff39cd1b067646d71784957eaa4200
mssj2008 <mohammadsamjalili@gmail.com>
2024-12-07 19:03:12 +0000

    hehe commit

--- my BIO/2022/Q3.cpp
+++ my BIO/2022/Q3.cpp
@@ -74,58 +74,32 @@ vector<int> get_path(vector<vector<ull>> &combos,vector<ull> &multi,ull index,ul
     return path;
 }
 
-vector<vector<ull>> get_combos_2(string park,int f1,int f2)
-{
-	vector<vector<ull>> combos;
-    string park2(park.length(),' ');
-    vector<int> cpark(park.length());
-    for(int i = 0; i < park.length(); i++)
-    {
-        cpark[park[i]-'a'] = i;
-    }
-    combos.push_back({cpark[0]});
-    park2[cpark[0]] = 'a';
-    //cout << cpark[0] << " ";
-    for(int c = 1; c < cpark.size(); c++)
-    {
-        //cout << cpark[c] << " ";
-        combos.push_back({cpark[c]});
-        if(c == f1-'a' || c == f2-'a')
-            continue;
-        for(int i = cpark[c]-1; park2[i]!=' ' && i >= 0; i--)
-        {
-            combos.back().push_back(i);
-        }
-        sort(combos.back().begin(),combos.back().end());
-        park2[cpark[c]] = c+'a';
-    }
-    //cout << endl;
-    return combos;
-}
-
-
 int main()
 {
     //ios_base::sync_with_stdio(false);
     //cin.tie(NULL);
 
-    string s = "abcdefghijklmnop";
-    ull idx = 0;
-    vector<vector<ull>> c;
-    for(int i = 'b'; i < 'q'; i++)
-    {
-        for(int j = i+1; j < 'q'; j++)
-        {
-            c = get_combos_2(s,i,j);
-            idx+=get_multi(c)[0];
-        }
-    }
-    cout << idx << endl;
-    return 0;
-    //cin >> s >> idx;
+    string s;
+    ull idx;
+    cin >> s >> idx;
     vector<vector<ull>> combos = get_combos(s);
+    //for(auto i : combos)
+    //{
+        //for(auto j : i)
+            //cout << j << " ";
+        //cout << endl;
+    //}
+    //cout << endl;
+    //cout << "combo done" << endl;
     vector<ull> multi = get_multi(combos);
+    //for(ull i : multi)
+    //{
+        //cout << i << " ";
+    //}
+    //cout << endl;
+    //cout << "multi done" << endl;
     vector<int> path = get_path(combos,multi,idx);
+    //cout << "path generated" << endl;
     string com;
     for(int i : path)
     {
@@ -137,3 +111,18 @@ int main()
     return 0;
 }
 
+/*
+
+didnt track time... :/ XD
+the osltuoin i saw from vid made me clock the idea
+took to fucking long to imeplemnt idfk why... :/
+but ye
+My problem was i thought the ocmibnations would change dpending
+on other car prefercens but really they dont(they come in order...)
+you jsut have to look at the eariler cars in the que and form that
+really jsut calcuate the number of possibilites of tht shit
+then simply count em up until u get to the index the input asked for
+(u start with early cars so the number are large so it does it qucikly no maatter wht)
+***Very similar to the integer string from 2024 Q1...
+
+*/
